792b820457b57a5e9dc4fde1b7fded0aa90fd5a1
belalhanafy <belalhanafy373@gmail.com>
2024-11-28 08:03:58 +0200

    update

--- src/components/Benifits.jsx
+++ src/components/Benifits.jsx
@@ -2,7 +2,7 @@ import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
-import { GradientLight } from "./design/Benefits";
+import { GradientLight } from "./Design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
